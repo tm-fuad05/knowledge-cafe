55addda38811aa0c3b05ae5e3b59f344f3ba97a5
tm-fuad05 <tm.fuad05@gmail.com>
2024-10-23 05:17:40 +0600

    change

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,17 @@ function App() {
     setBookmarks(newBookmarks);
   };
   return (
-    <div className="w-11/12 mx-auto">
+    <>
       <Header></Header>
-      <div className="grid grid-cols-3 gap-5">
-        <Blogs handleBookmark={handleBookmark}></Blogs>
 
-        <Bookmarks bookmarks={bookmarks}></Bookmarks>
+      <div className="w-11/12 lg:w-10/12 md:w-10/12 mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-5">
+          <Blogs handleBookmark={handleBookmark}></Blogs>
+
+          <Bookmarks bookmarks={bookmarks}></Bookmarks>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Profile from "../assets/profile.png";
 export default function Header() {
   return (
-    <nav className="my-5 flex justify-between items-center border-b pb-2 ">
+    <nav className="my-5 flex justify-between items-center border-b pb-2 w-11/12 md:w-11/12 lg:w-11/12 mx-auto">
       <h2 className="text-3xl font-bold">Knowledge Cafe</h2>
       <button className="border border-sky-400 rounded-full p-1 w-12 h-12">
         <img className="w-full h-full " src={Profile} alt="" />
